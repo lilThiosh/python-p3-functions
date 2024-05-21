c24d58af84c7308cd2738ab8994eac1cf1553e55
Ian <thiongoian6148@gmail.com>
2024-05-21 14:44:06 +0300

    Rewriting Js functions to Python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print (f"Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name = "Naureen"):
+    print (f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
+    return (num1 + num2)
+
+    sum = add(1,2)
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+      return None
+    return number/2
+print(halve(100))
